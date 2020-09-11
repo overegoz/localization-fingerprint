c76e85bac29d115e179943e3ce286d2ca6e2ac4d
overegoz <twoon.kim@gmail.com>
2020-09-11 09:48:47 +0900

    revised

--- common.py
+++ common.py
@@ -6,5 +6,6 @@ Define some commonly-used values
 server_ip = "192.168.0.2"
 server_port = 9999
 BUF_SIZE = 1024
+tar_name = 'archieve.tar.gz'
 
 # THE END
--- rpi/upload-setup.py
+++ rpi/upload-setup.py
@@ -4,15 +4,15 @@ import socket
 
 if __name__ == "__main__":
 
-    # RPi 가 측정한 신호세기가 저장한 파일이 들어있는 폴더 전체를 압축
-    dir_name = 'measure-1'
-    tar_name = 'archieve.tar.gz'
-    tar_compress_cmd = 'tar -cvf ' + tar_name + ' ' + dir_name
-    os.system(tar_compress_cmd)
-    assert os.path.isfile(tar_name), "Tar file does not exist!"
-
     sys.path.append('../')
     import common
+
+    # RPi 가 측정한 신호세기가 저장한 파일이 들어있는 폴더 전체를 압축
+    dir_name = 'measure-1'
+    tar_compress_cmd = 'tar -cvf ' + common.tar_name + ' ' + dir_name
+    os.system(tar_compress_cmd)
+    assert os.path.isfile(common.tar_name), "Tar file does not exist!"
+
 
     # 앞축 파일을 서버로 보내기
     cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -20,14 +20,15 @@ if __name__ == "__main__":
 
     try:
         cli_socket.connect((common.server_ip, common.server_port))
+        print('Connected to the server...')
 
     except:
         assert False, 'Stop : socket connect error'
 
     # 파일 내용 보내기
-    print('Sending file now...')
     with open(tar_name, 'rb') as fp:
         while True:
+            print('Sending file now...')
             data_to_send = fp.read(BUF_SIZE)
             if not data_to_send:
                 break
