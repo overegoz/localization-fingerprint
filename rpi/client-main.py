1327b67dcdeb6047e8a94dc0baabf04a4c794f7b
overegoz <twoon.kim@gmail.com>
2020-09-11 13:50:19 +0900

    cli done

--- rpi/client-main.py
+++ rpi/client-main.py
@@ -15,20 +15,22 @@ if __name__ == "__main__":
     out_filename_base = out_dir + '/' + 'client-measure-realtime'
     wifi_dev_name = 'wlan0'
     scan_cmd_base =  "sudo iwlist " + wifi_dev_name + " scan | grep -E 'level|Address' | sed 's/level=//' | awk '{ if ( $1 == \"Cell\" ) { print $5 } if ( $2 == \"Signal\" ) { print $3 } }'"
-    
+   
+    # 클라이언트 소켓 생성   
     cli_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     print('Connecting to...', common.server_ip, server_port)
 
+    # 서버에 연결 시도
     try:
         cli_socket.connect((common.server_ip, common.server_port))
         print('Connected to the server...')
     except:
         assert False, 'Stop: socket connection error'
 
-    tx_counter=0
+    tx_counter=0  # 서버로 몇번 전송했는지를 계산할 카운터 변수
     try:
         while True:
-            print('Counter : ', tx_counter)
+            print('Counter : ', tx_counter)  # 디버깅을 위한 터미널 출력
 
             # 근처의 모든 AP의 RSS값을 스캔해서 파일로 저장
             out_filename_now = out_filename_base + common.delimiter + str(tx_counter) + '.txt'
@@ -37,7 +39,6 @@ if __name__ == "__main__":
     
             # 저장한 파일을 읽어서, 메시지로 변환 후 서버로 전송
             msg2send = get_msg2send(out_filename_now)
-
             cli_socket.sendall(bytes(msg2send, "utf-8"))
             
             # 1초간 대기
